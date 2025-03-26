552e1039ae7a45b1ea38b9eeec27c8a05417c84d
darcy-stubbins <darcystubbins.dev.99@gmail.com>
2025-03-26 16:02:46 +0000

    created price column in drinks table and seeded it

--- database/seeders/DrinkSeeder.php
+++ database/seeders/DrinkSeeder.php
@@ -16,42 +16,49 @@ class DrinkSeeder extends Seeder
         new Drink([
             'drink_type_id' => '1',
             'name' => 'Breakfast Tea',
+            'price' => '01.00',
             'description' => 'This is breakfast tea',
         ])->save();
 
         new Drink([
             'drink_type_id' => '1',
             'name' => 'Green Tea',
+            'price' => '1.20',
             'description' => 'Tea but green',
         ])->save();
 
         new Drink([
             'drink_type_id' => '2',
             'name' => 'Americano',
+            'price' => '1.50',
             'description' => 'Black coffee',
         ])->save();
 
         new Drink([
             'drink_type_id' => '2',
             'name' => 'Latte',
+            'price' => '2.00',
             'description' => 'Better than americano',
         ])->save();
 
         new Drink([
             'drink_type_id' => '2',
             'name' => 'Mocha',
+            'price' => '2.50',
             'description' => 'The best coffee',
         ])->save();
 
         new Drink([
             'drink_type_id' => '3',
             'name' => 'Chai Tea',
+            'price' => '1.20',
             'description' => 'Tea version of chai',
         ])->save();
 
         new Drink([
             'drink_type_id' => '3',
             'name' => 'Chai Latte',
+            'price' => '2.50',
             'description' => 'Latte version of chai',
         ])->save();
     }
